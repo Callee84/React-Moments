68af1d99c54a25562c373ec12531727dab18d1e8
Callee84 <carl.g.holm@gmail.com>
2022-12-22 20:24:24 +0000

    Add more routes and style to navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+        <Navbar.Brand><img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink 
+              exact 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} to="/">
+              <i className="fas fa-home"/>Home
+            </NavLink>
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} to="/signin">
+              <i className="fas fa-sign-in-alt"/>Sign in
+            </NavLink>
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} to="signout">
+              <i className="fas fa-user-plus"/>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
